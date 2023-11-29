2f3eb1480bc40416885c15068188e841c1505ae9
van khin <khmaister@yahoo.com>
2023-11-28 21:00:50 -0500

    basic handlebar template

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Handlebars as the default template engine.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
 // Starts the server to begin listening
 app.listen(PORT, () => {
     console.log('Server listening on: http://localhost:' + PORT);
