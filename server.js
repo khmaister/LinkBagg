a9e2c56197ea1e118d1a2b5e0671e4b14004ff6b
van khin <khmaister@yahoo.com>
2023-12-04 21:27:37 -0500

    successful page render on insomnia

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // Dependencies
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const hbs = exphbs.create({});
 
 // Set up the Express App
 const app = express();
@@ -8,8 +11,8 @@ const PORT = process.env.PORT || 3001;
 // Handlebars as the default template engine.
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/user-routes'));
 // Starts the server to begin listening
 app.listen(PORT, () => {
     console.log('Server listening on: http://localhost:' + PORT);
